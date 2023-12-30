121fcaca704af8ebb7af983f02dc533139d3e49e
Subhadip Dutta <subhadip.dutta.18@gmail.com>
2023-12-30 14:35:13 +0530

    Initial Code

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
+
 # Text_Guru_Website
+
 This is a small flask application that takes the whatsapp chat export as data to analyze the sentiments of the conversation as well as give some interesting highlight.In future want to add more complex models to do an accurate emotional and relationship index of the conversations
+
+
+## Demo & Screenshots
+
+Home Screen
+![App Screenshot](https://i.imgur.com/v7fj3WK.png)
+
+User needs to export whatsapp chat and upload it.
+
+Result Screen
+![App Screenshot](https://i.imgur.com/6zZ2RDp.png)
+
+Result and Insights are seen on this screen.
+
+
+
+## Installation
+
+Deploy my project in Cloud using Docker
+
+```bash
+Build Docker Image
+docker build -t python-docker . 
+
+Run docker container from image
+docker container run -dit -p 443:443 python-docker
+
+Delete all image,container and volumes
+docker system prune -a --volumes
+```
+
+Run locally
+
+```bash
+Conda create a virtual environment
+conda create -n myenv python=3.9
+
+Activate conda environment
+conda activate myenv
+
+Install requirements packages
+pip install -r requirements.txt 
+
+Run app.py
+python app.py
+```
+
+## Roadmap
+
+- Add more complex sentiment models and make the outputs more meaningfull about personality traits and the type of person
+
+- Add more integrations with SQL database to give the personality traits from multiple conversation for a profile
+
+- Add sign in and profile pages and also add a social integration
+
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [Subhadip Dutta]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ app.py
@@ -0,0 +1,188 @@
+import os
+import re
+from flask import Flask, render_template, request
+import pandas as pd
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import warnings
+
+from collections import Counter
+from datetime import datetime
+import random
+import nltk
+
+app = Flask(__name__)
+
+nltk.download('vader_lexicon')
+sentiments = SentimentIntensityAnalyzer()
+
+# Suppress the UserWarning related to date parsing
+warnings.filterwarnings("ignore", category=UserWarning)
+
+def extract_chat_info(file_path):
+    participants = set()
+    chat_dates = []
+    total_chats = 0
+
+    with open(file_path, encoding="utf-8") as file:
+        for line in file:
+            line = line.strip()
+
+            # Extract participants
+            match = re.match(r'^[0-9/]+, [0-9:]+ - (.*?):', line)
+            if match:
+                participants.add(match.group(1))
+
+            # Extract chat dates
+            date_match = re.match(r'^([0-9/]+), [0-9:]+ -', line)
+            if date_match:
+                chat_date = datetime.strptime(date_match.group(1), "%d/%m/%y").date()
+                chat_dates.append(chat_date)
+
+            total_chats += 1
+
+    # Count occurrences of each date
+    date_counter = Counter(chat_dates)
+    most_chats_day = date_counter.most_common(1)[0][0].strftime("%A")
+
+    start_date = min(chat_dates).strftime("%B %d, %Y")
+
+    return {
+        'participants': ' & '.join(participants),
+        'start_date': start_date,
+        'total_chats': total_chats,
+        'most_chats_day': most_chats_day
+    }
+
+def extract_random_chat(file_path):
+    data = []
+    with open(file_path, encoding="utf-8") as fp:
+        fp.readline()
+        message_buffer = []
+        date, time, author = None, None, None
+        while True:
+            line = fp.readline()
+            if not line:
+                break
+            line = line.strip()
+            if date_time(line):
+                if len(message_buffer) > 0:
+                    # Concatenate (Date) Author : Message
+                    chat = f"({date}) {author} : {' '.join(message_buffer)}"
+                    data.append(chat)
+                message_buffer.clear()
+                date, time, author, message = get_datapoint(line)
+                message_buffer.append(message)
+            else:
+                message_buffer.append(line)
+
+    # Randomly select 10 consecutive chats
+    start_index = random.randint(0, len(data) - 20)
+    selected_chats = data[start_index:start_index + 20]
+
+    return selected_chats
+
+def analyze_sentiment(file_path):
+    data = []
+    with open(file_path, encoding="utf-8") as fp:
+        fp.readline()
+        message_buffer = []
+        date, time, author = None, None, None
+        while True:
+            line = fp.readline()
+            if not line:
+                break
+            line = line.strip()
+            if date_time(line):
+                if len(message_buffer) > 0:
+                    data.append([date, time, author, ' '.join(message_buffer)])
+                message_buffer.clear()
+                date, time, author, message = get_datapoint(line)
+                message_buffer.append(message)
+            else:
+                message_buffer.append(line)
+
+    df = pd.DataFrame(data, columns=["Date", 'Time', 'Author', 'Message'])
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    # Set values using .loc to avoid the warning
+    df.loc[:, "Compound"] = [sentiments.polarity_scores(i)["compound"] for i in df["Message"]]
+
+    overall_sentiment = sum(df["Compound"])
+
+    sentiment_result = None
+    if overall_sentiment >= 0.05:
+        sentiment_result = "Positive 😊"
+    elif overall_sentiment <= -0.05:
+        sentiment_result = "Negative 😠"
+    else:
+        sentiment_result = "Neutral 🙂"
+
+    return round(overall_sentiment,0), sentiment_result
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return render_template('index.html', error='No file part')
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return render_template('index.html', error='No selected file')
+
+        if file:
+            file_path = os.path.join('uploads', file.filename)
+            file.save(file_path)
+
+            overall_sentiment, sentiment_result = analyze_sentiment(file_path)
+            chat_info = extract_chat_info(file_path)
+            #print(chat_info)
+            # Replace this with your logic to get random chat moments
+            random_chat_moments = [
+                "Hey, how are you?",
+                "Check out this funny meme!",
+                "Remember that time we went to the beach?",
+                # Add more chat moments
+            ]
+            ran_chat = extract_random_chat(file_path)
+            #print(ran_chat)
+
+            return render_template('result.html', overall_sentiment=overall_sentiment, sentiment_result=sentiment_result,overall_sentiment_percentage=overall_sentiment,chat_info=chat_info,random_chat_moments=ran_chat)
+
+    return render_template('index.html', error=None)
+
+def date_time(s):
+    pattern = '^([0-9]+)(\/)([0-9]+)(\/)([0-9]+), ([0-9]+):([0-9]+)[ ]?(AM|PM|am|pm)? -'
+    result = re.match(pattern, s)
+    if result:
+        return True
+    return False
+
+def find_author(s):
+    s = s.split(":")
+    if len(s) == 2:
+        return True
+    else:
+        return False
+
+def get_datapoint(line):
+    split_line = line.split(' - ')
+    date_time = split_line[0]
+    date, time = date_time.split(", ")
+    message = " ".join(split_line[1:])
+    if find_author(message):
+        split_message = message.split(": ")
+        author = split_message[0]
+        message = " ".join(split_message[1:])
+    else:
+        author = None
+    return date, time, author, message
+
+if __name__ == '__main__':
+    os.makedirs('uploads', exist_ok=True) 
+    HOST = '0.0.0.0'
+    PORT = 80
+    #PORT = 443
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.run(host=HOST, port=PORT, debug=True)
